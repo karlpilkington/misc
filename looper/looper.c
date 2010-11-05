80d0757efd7a6a9130b6165e9af33e004095f66b
Troy D. Hanson <tdh@tkhanson.net>
2010-11-04 19:26:55 -0700

    Cleaned up this example program for file line by line iteration

--- looper/looper.c
+++ looper/looper.c
@@ -4,41 +4,28 @@
 #include <stdlib.h>
 #include <unistd.h>
  
-int n=16;
-int verbose=0;
-char *ofile=NULL;
-char *ifile=NULL;
- 
+/*
+ * This is a simple template for a program that opens a file (or reads from
+ * stdin if no file is specified), then iterates over the lines of a file.
+ */
+
 void usage(char *prog) {
-  fprintf(stderr, "usage: %s [-n <num>] [-o <outfile>] [-v]\n", prog);
+  fprintf(stderr, "usage: %s [-v] <file>\n", prog);
   exit(-1);
 }
  
 int main(int argc, char * argv[]) {
-  int opt;
+  int opt,verbose=0,lines=0;
   FILE *ifilef=stdin;
-  char line[100];
+  char *ifile=NULL,line[100];
  
-  while ( (opt = getopt(argc, argv, "n:o:v+")) != -1) {
+  while ( (opt = getopt(argc, argv, "v+")) != -1) {
     switch (opt) {
-      case 'n':
-        n = atoi(optarg);
-        break;
-      case 'o':
-        ofile = strdup(optarg);
-        break;
-      case 'v':
-        verbose++;
-        break;
-      default:
-        usage(argv[0]);
-        break;
+      case 'v': verbose++; break;
+      default: usage(argv[0]); break;
     }
   }
- 
   if (optind < argc) ifile=argv[optind++];
-  fprintf(stderr,"n %u, ofile %s, verbose %u\n", n, ofile?ofile:"none",verbose);
-  fprintf(stderr,"ifile %s\n", ifile?ifile:"stdin");
  
   /* loop over the input */
   if (ifile) {
@@ -49,6 +36,8 @@ int main(int argc, char * argv[]) {
   }
  
   while (fgets(line,sizeof(line),ifilef) != NULL) {
-    fprintf(stderr, "read line: %s", line);
+    lines++;
+    if (verbose) fprintf(stderr, "read line: %s", line);
   }
+  printf("%d lines read.\n",lines);
 }
