5e026f5ab48b33295aff96384a0cae8071d73f44
Troy D. Hanson <tdh@tkhanson.net>
2010-11-09 19:21:53 -0500

    wip on tconf

--- tconf/tconf.c
+++ tconf/tconf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 #include "tconf.h"
 
@@ -20,15 +21,41 @@ int tconf(char *file, tconf_t *tconf, int tclen, int opt) {
   }
 
   while (fgets(line,sizeof(line),f) != NULL) {
-    k=line; while(ws[*k]) k++;                           /* trim space */
-    if ((*k == '#') || nl[*k] || *k == '\0') continue;   /* skip comments */
-    v=k; while(!(ws[*v] || nl[*v] || *v=='\0')) v++;     /* find k/v delim */
+    k=line; while(ws[*k]) k++;                           /* trim pre space */
+    if ((*k == '#') || nl[*k] || (*k=='\0')) continue;   /* skip comments */
+    v=k; while(!(ws[*v] || nl[*v] || (*v=='\0'))) v++;   /* find k/v delim */
     klen = v-k;
-    if (*v != '\0') v++;
-    vlen = 0; while (!(nl[*v] || (*v=='\0'))) vlen++;
+    while(ws[*v]) v++;                                   /* trim pre space */
+    vlen = 0; while (!(nl[*(v+vlen)] || (*(v+vlen)=='\0'))) vlen++;
+    while(vlen && ws[*(v+vlen-1)]) vlen--;               /* trim post space */
 
     for(t = tconf; t < tconf+tclen; t++) {
-      //if (t->name
+      if ((klen != strlen(t->name)) || (strncmp(k,t->name,klen))) continue;
+      switch(t->type) {
+        case tconf_bool:
+          if (vlen) {
+            if (sscanf(v,"%d",(int*)(t->addr)) != 1) goto done;
+            *(int*)(t->addr) = (*(int*)(t->addr))  ? 1 : 0;
+          } else {
+            *(int*)(t->addr) = 1;  /* lone key name means boolean true */
+          }
+          break;
+        case tconf_int:
+          if (!vlen) goto done;
+          if (sscanf(v,"%d",(int*)(t->addr)) != 1) goto done;
+          break;
+        case tconf_str:
+          if (!vlen) goto done;
+          if ( (*(char**)(t->addr) = malloc(vlen+1)) == NULL) goto done;
+          strncpy(*(char**)(t->addr),v,vlen);
+          (*(char**)(t->addr))[vlen]='\0';
+          break;
+        case tconf_func:
+          break;
+        default:
+          fprintf(stderr,"unknown tconf type %d\n",tconf->type);
+          goto done;
+      }
     }
   }
 
