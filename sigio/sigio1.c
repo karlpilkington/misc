3f08825c47064040a49eb3f2d38df37a98f6cfa9
Troy D. Hanson <tdh@tkhanson.net>
2011-03-15 21:37:43 -0400

    works

--- sigio/sigio1.c
+++ sigio/sigio1.c
@@ -6,50 +6,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <setjmp.h>
 
 sigjmp_buf jmp;
 /* signals that we'll unblock during sigsuspend */
-int sigs[] = {0,SIGHUP,SIGCHLD,SIGTERM,SIGINT,SIGQUIT,SIGALRM};
+int sigs[] = {SIGIO,SIGHUP,SIGCHLD,SIGTERM,SIGINT,SIGQUIT,SIGALRM};
 
-volatile int sigfd; /* just for sanity checking - should match fd */
-void sighandler(int signo, siginfo_t *info, void *ucontext) {
-  sigfd = info->si_fd; /* see sigaction(2), inotify(7) */
+void sighandler(int signo) {
   siglongjmp(jmp,signo);
 }
 
+void spawn_child(int fd[2], char *greeting, char *goodbye) {
+  if (fork() != 0) return;  /* parent */
+  close(fd[0]);
+  sleep(1); write(fd[1],greeting,strlen(greeting));
+  sleep(1); write(fd[1],goodbye, strlen(goodbye));
+  close(fd[1]);
+  exit(-1);
+}
+
 int main(int argc, char *argv[]) {
-  int fd=0, rc, n;
-  char buf[10];
-  sigs[0] = SIGRTMIN+0;
+  int fd[2], rc, n;
+  char buf[100];
 
-  /* set up signal I/O notification for fd; see fcntl(2) */
-  int fl = fcntl(fd, F_GETFL);
-  fl |= O_ASYNC;
-  if (fcntl(fd, F_SETFL, fl) == -1) {
-    perror("fcntl O_ASYNC failed");
-    exit(-1); 
-  }
+  pipe(fd);
+  spawn_child(fd,"hello\n","world\n");
+  close(fd[1]);
 
-  /* now we use fcntl(fd,F_SETSIG,SIGRTMIN) etc to change
-   * the signal we want when fd is ready, instead of SIGIO. Why?
-   * Because these other signals will receive "additional info"
-   * (in particular, the fd that is ready) if that signal handler
-   * is installed with SA_SIGINFO. But NOTE that only if the
-   * chosen signal number is an RT signal (>= SIGRTMIN) will
-   * these signal events be QUEUED; this is necessary to get
-   * differentiable notifications for _different_ fd's all using
-   * one signal. */
-  if (fcntl(fd, F_SETSIG, sigs[0]) == -1) {
-    perror("fcntl F_SETOWN failed");
-    exit(-1); 
-  }
-   /* if this were a multi-threaded program, the previous call
-    * also sets the thread recipient; see fcntl(2) */
-  if (fcntl(fd, F_SETOWN, getpid()) == -1) {
-    perror("fcntl F_SETOWN failed");
-    exit(-1); 
-  }
+  /* request SIGIO to our pid when fd[0] is ready; see fcntl(2) */
+  int fl = fcntl(fd[0], F_GETFL);
+  fl |= O_ASYNC | O_NONBLOCK;
+  fcntl(fd[0], F_SETFL, fl);
+  fcntl(fd[0], F_SETOWN, getpid()); 
 
   /* block all signals. we stay blocked always except in sugsuspend */
   sigset_t all;
@@ -63,22 +52,32 @@ int main(int argc, char *argv[]) {
 
   /* establish handlers for signals that'll be unblocked in sigsuspend */
   struct sigaction sa;
-  sa.sa_sigaction=sighandler; 
-  sa.sa_flags=SA_SIGINFO;
+  sa.sa_handler=sighandler;  /* no fd information (not sa_sigaction)  */
+  sa.sa_flags=0;             /* no extra information (not SA_SIGINFO* */
   sigfillset(&sa.sa_mask);
   for(n=0; n < sizeof(sigs)/sizeof(*sigs); n++) sigaction(sigs[n], &sa, NULL);
 
   /* here is a special line. we'll come back here whenever a signal happens */
   int signo = sigsetjmp(jmp,1);
 
-  if (signo == 0) { 
-    /* initial setup, no signal yet */
-  } else if (signo == sigs[0]) {
-    printf("siginfo indicated fd %d (expected %d)\n", sigfd, fd);
-    if ( (rc = read(fd, buf, sizeof(buf))) <= 0) goto done;
-  } else {
-    printf("got signal %d\n", signo);
-    goto done;
+  switch (signo) {
+    case 0:
+      /* initial setup. no signal yet */
+      break;
+    case SIGCHLD:
+      /* this can happen before we get the last SIGIO,
+         so fall through to grab any final pipe content */
+      printf("got sigchld\n");
+    case SIGIO:
+      rc = read(fd[0], buf, sizeof(buf));
+      if (rc > 0) printf("read: %.*s", rc, buf);
+      else if (rc == 0) { close(fd[0]); goto done; }
+      else if (rc == -1) {perror("read error"); close(fd[0]);}
+      break;
+    default:
+      printf("got signal %d\n", signo);
+      goto done;
+      break;
   }
 
   /* wait for signals */
