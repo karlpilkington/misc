9f00f3cb38bd7d94abb7e75740e8a671ddbb0f2a
Troy D. Hanson <tdh@tkhanson.net>
2011-07-03 04:00:00 -0400

    compiles

--- controlport/controlport.h
+++ controlport/controlport.h
@@ -4,23 +4,25 @@
 #include <stdlib.h>
 
 typedef struct {
-  int     argc;      /* conventional argc/argv args */
+  int     argc; /* conventional argc/argv args */
   char  **argv;
-  int     bin_argc;  /* binary buffer arguments */
-  size_t *bin_lenv;
-  char  **bin_argv;
+  size_t *lenv; // len of each arg, useful if client sends null-containing args
 } cp_arg_t;
 
-typedef int (cp_cmd_f)(cp_arg_t *arg, cp_arg_t *out);
+typedef int (cp_cmd_f)(void *cp, cp_arg_t *arg, void *data);
 
 typedef struct {
   char *name;
   cp_cmd_f *cmdf;
 } cp_cmd_t;
 
-void *cp_init(char *path, cp_cmd_t *cmds, int timeout);
-int cp_add_cmd(void*, cp_cmd_t *cmd);
-int cp_start(void*);
-void cp_push_binarg(cp_arg_t *arg, void *buf, size_t len, int do_copy);
+void *cp_init(char *path, cp_cmd_t *cmds, void *data, int timeout);
+void cp_add_cmd(void*, char *name, cp_cmd_f *cmdf, void *data);
+int cp_run(void*);
+void cp_shutdown(void*); /* callable from another thread */
+
+/* these are used within command callbacks */
+void cp_disconnect(void*);
+void cp_add_reply(void *, void *buf, size_t len);
 
 #endif 
