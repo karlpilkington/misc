79c4295ee53a72e5e151dcf57fd6f6fdd1bb5aed
Troy D. Hanson <tdh@tkhanson.net>
2011-12-08 08:07:43 -0500

    wip

--- web/008.isometric/core.js
+++ web/008.isometric/core.js
@@ -2,17 +2,19 @@
 $(document).ready(startup);
 
 function startup() {
-  window.alert("here");
   setInterval(roll, 1000);
 }
 
 var ts = 0;
 var alpha_degrees = 30.0;
 var beta_degrees = 30.0;
-var cos_alpha = Math.cos(alpha_degrees/360 * 2*Math.PI);
-var sin_alpha = Math.sin(alpha_degrees/360 * 2*Math.PI);
-var cos_beta = Math.cos(beta_degrees/360 * 2*Math.PI);
-var sin_beta = Math.sin(beta_degrees/360 * 2*Math.PI);
+var xs=1;
+var ys=1;
+var zs=1;
+var cos_alpha = xs * Math.cos(alpha_degrees/360 * 2*Math.PI);
+var sin_alpha = xs * Math.sin(alpha_degrees/360 * 2*Math.PI);
+var cos_beta =  ys * Math.cos(beta_degrees/360 * 2*Math.PI);
+var sin_beta =  ys * Math.sin(beta_degrees/360 * 2*Math.PI);
 var rows = [];
 
 function iso(d,t) {
@@ -20,19 +22,17 @@ function iso(d,t) {
   // we generate a polyline string using an isometric projection
   var y = t; // 0 <= y <= 9
   var x;
-  var xs=10;
-  var ys=10;
-  var zs=1;
   var height = 10*xs + 10*ys + 10*zs;
   var polyline = "";
   for(x=0; x<10; x++) {
     var z = d.v[x];
-    var u = x*xs*cos_alpha - y*ys*cos_beta;
-    var v = x*xs*sin_alpha + y*ys*sin_beta + z*zs;
+    var u = Math.floor(x*cos_alpha - y*cos_beta);
+    var v = Math.floor(x*sin_alpha + y*sin_beta + z*zs);
     v = height - v; // flip v for SVG (origin at upper-left)
     // append to polyline
     polyline = polyline + (polyline.length ? "," : "") + u + "," + v;
   }
+  return polyline;
 }
 
 function roll() {
@@ -46,14 +46,16 @@ function roll() {
   if (rows.length == 10) rows.shift(); // delete oldest row
   rows.push(r); // add new row
 
-  var viz = d3.select("#viz").selectAll("svg:polyline")
+  var viz = d3.select("#viz").selectAll("polyline")
    .data(rows, function(d) {return d.ts});
 
   viz.enter().append("svg:polyline")
-   .points(iso);
+   .attr("points",iso)
+   .style("stroke", "blue")
+   .style("stroke-width", 3);
 
   viz.transition()
-   .points(iso);
+   .attr("points",iso);
 
   viz.exit().remove();
 }
