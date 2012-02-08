363a5f34b24ab9a5ed6dd92348d5a4ecdef38b24
Troy D. Hanson <tdh@tkhanson.net>
2012-02-07 20:56:44 -0500

    sample of perl webserver

---
+++ web/perl/websrv.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+# run script and use browser to visit 
+# http://localhost:8080/hello
+
+# depends on HTTP::Server::Simple
+# install on Ubuntu using 
+# sudo apt-get install libhttp-server-simple-perl
+#
+# this example is from perldoc HTTP::Server::Simple
+
+{
+package MyWebServer;
+
+use HTTP::Server::Simple::CGI;
+use base qw(HTTP::Server::Simple::CGI);
+
+my %dispatch = (
+    '/hello' => \&resp_hello,
+    # ...
+);
+
+sub handle_request {
+    my $self = shift;
+    my $cgi  = shift;
+
+    my $path = $cgi->path_info();
+    my $handler = $dispatch{$path};
+
+    if (ref($handler) eq "CODE") {
+        print "HTTP/1.0 200 OK\r\n";
+        $handler->($cgi);
+
+    } else {
+        print "HTTP/1.0 404 Not found\r\n";
+        print $cgi->header,
+              $cgi->start_html('Perl mini webserver'),
+              $cgi->h1('Not found'),
+              $cgi->end_html;
+    }
+}
+
+sub resp_hello {
+    my $cgi  = shift;   # CGI.pm object
+    return if !ref $cgi;
+
+    my $who = $cgi->param('name');
+
+    print $cgi->header,
+          $cgi->start_html("Hello"),
+          $cgi->h1("Hello $who!"),
+          $cgi->h2("Your IP address is ", $cgi->remote_addr()),
+          $cgi->end_html;
+}
+
+}
+
+# start the server on port 8080
+#my $pid = MyWebServer->new(8080)->background();
+#print "Use 'kill $pid' to stop server.\n";
+my $pid = MyWebServer->new(8080)->run(); #foreground mode
